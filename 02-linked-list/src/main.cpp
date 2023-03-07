8772d6963d76616979b1fb658936f4eff4a14302
morgan bergen <morgan@ku.edu>
2023-03-07 09:26:46 -0600

    examples of prefix and postfix increments

--- 02-linked-list/src/main.cpp
+++ 02-linked-list/src/main.cpp
@@ -5,8 +5,39 @@
 
 int main() {
     
-    // LinkedList<std::string> green;
+    LinkedList<int> first;
 
-    LinkedList<int> green;
+    int x = 10;
 
-    return(0); }
+    std::cout << x << std::endl;
+    std::cout << x++ << std::endl;
+    std::cout << x << std::endl;
+    
+    x = 10;
+
+    std::cout << x << std::endl;
+    std::cout << ++x << std::endl;
+    std::cout << x << std::endl;
+    
+    /*
+     * test push front function
+     * green.push_front(data);
+     *
+     * test empty
+     * green.empty();
+     *
+     * test size
+     * green.size();
+     *
+     * test front
+     * green.front();
+     *
+     * test back
+     * green.back();
+     *
+     * test the construction functyion
+     * LinkedList<std::string> second = LinkedList<std::string>(LinkedList<std::string>(first));
+     */
+
+    return(0); 
+}
