5dcaf4fd8efba3fe6c805960e65e7fe3d78e30b0
morgan bergen <morgan@ku.edu>
2023-03-24 07:23:14 -0500

    updated hash tree

--- src/04-hash-table/00-provided/src/MyInfixCalculator.h
+++ src/04-hash-table/00-provided/src/MyInfixCalculator.h
@@ -11,18 +11,15 @@ class MyInfixCalculator{
 
   public:
     
-    MyInfixCalculator()
-    {
+    MyInfixCalculator() {
 
     }
 
-    ~MyInfixCalculator()
-    {
+    ~MyInfixCalculator() {
      
     }
 
-    double calculate(const std::string& s)
-    {
+    double calculate(const std::string& s) {
         // code begins
         return(55.5);
         // code ends
@@ -33,10 +30,8 @@ class MyInfixCalculator{
     // returns operator precedance; the smaller the number the higher precedence
     // returns -1 if the operator is invalid
     // does not consider parenthesis
-    int operatorPrec(const char c) const
-    {
-        switch(c)
-        {
+    int operatorPrec(const char c) const {
+        switch(c) {
             case '*':
                 return 2;
             case '/':
@@ -51,10 +46,8 @@ class MyInfixCalculator{
     }
 
     // checks if a character corresponds to a valid parenthesis
-    bool isValidParenthesis(const char c) const
-    {
-        switch(c)
-        {
+    bool isValidParenthesis(const char c) const {
+        switch(c) {
             case '(':
                 return true;
             case ')':
@@ -65,20 +58,17 @@ class MyInfixCalculator{
     }
 
     // checks if a character corresponds to a valid digit
-    bool isDigit(const char c) const
-    {
+    bool isDigit(const char c) const {
         if(c >= '0' && c <= '9')
             return true;
         return false;
     }
 
     // computes binary operation given the two operands and the operator in their string form
-    double computeBinaryOperation(const std::string& ornd1, const std::string& ornd2, const std::string& opt) const
-    {
+    double computeBinaryOperation(const std::string& ornd1, const std::string& ornd2, const std::string& opt) const {
         double o1 = std::stod(ornd1);
         double o2 = std::stod(ornd2);
-        switch(opt[0])
-        {
+        switch(opt[0]) {
             case '+':
                 return o1 + o2;
             case '-':
@@ -95,16 +85,14 @@ class MyInfixCalculator{
 
 
     // tokenizes an infix string s into a set of tokens (operands or operators)
-    void tokenize(const std::string& s, MyVector<std::string>& tokens)
-    {
+    void tokenize(const std::string& s, MyVector<std::string>& tokens) {
         // code begins
 
         // code ends
     }
 
     // converts a set of infix tokens to a set of postfix tokens
-    void infixToPostfix(MyVector<std::string>& infix_tokens, MyVector<std::string>& postfix_tokens)
-    {
+    void infixToPostfix(MyVector<std::string>& infix_tokens, MyVector<std::string>& postfix_tokens) {
         // code begins
 
         // code ends
--- src/04-hash-table/README.md
+++ src/04-hash-table/README.md
@@ -5,7 +5,8 @@
 1.  [objective](#objective)
 2.  [specifications](#specifications)
 3.  [testing](#testing)
-
+4.  [program flow](#program-flow)
+5.  [`maintest.cpp`](#maintest-cpp)
 
 ## objective
 
@@ -14,5 +15,82 @@ understand the infix and postfix calculation using stack.
 ## specifications
 
 1.  only addition `+`, subtraction `-`, multiplication `*`, and division `/` are assumed to be valid operators from the infix input
+
 2.  only parenthesis `((` and `))` are assumed from the infix input
-3.  both integers and floating point numbers are assumed to be 
+
+3.  both integers and floating point numbers are assumed to be valid inputs
+
+4.  both positive and negative numbers are assumed to be valid inputs
+
+5.  the input infix string is balanced and valid, fully complied with customary mathematical notation
+
+6.  implement a class name `MyInfixCalaculator` the class is expected to use the `MyStack` and `MyVector` classes you have implemented previously.
+
+7.  implement `void tokenize(const std::string>& infix_tokens, MyVector<std::string>& postfix_tokems)` to decompose the infix string into a set of tokens
+
+8.  implement `void infixToPostfix(MyVector<std::string>& infix_tokens) const, MyVector<std::string>& postfix_tokens)` to convert a set of infix tokens to a set of postfix tokens 
+
+9.  implement `doubel calPostFix(const MyVector<std::string>& postfix_tokens) const` to calculate the result
+
+10. implement a wrapper function `double calculate(const std::string& s)` to directly compute the result from a given infix input.
+
+##  testing
+
+you will be tested over the following compiler under **Ubuntu 20.04 LTS using `g++` version 9.3.0   (default) with C++11 standard**
+
+## program flow
+
+the following shows the directory tree
+
+```
+❯ tree
+.
+├── MainTest.cpp            # 1st entry #include MyInfixCalc.h
+├── MyInfixCalculator.h
+├── MyStack.h
+├── MyVector.h
+├── README.txt
+├── Inputs
+│   ├── input_0.txt
+│   ├── ...
+│   └── input_9.txt
+├── Outputs
+│   ├── output_0.txt
+│   ├── ...
+│   └── output_9.txt
+└── GradingScript.py
+
+3 directories, 27 files
+```
+
+###  `MainTest.cpp`
+
+```cpp
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include <string>
+
+#include "MyInfixCalculator.h"
+
+
+
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
