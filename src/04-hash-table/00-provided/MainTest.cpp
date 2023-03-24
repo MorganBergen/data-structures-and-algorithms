ee70c2f52ab50bf20885b2e3d4e7baa1e59b0337
morgan bergen <morgan@ku.edu>
2023-03-24 05:29:21 -0500

    provided files

---
+++ src/04-hash-table/00-provided/MainTest.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include <string>
+
+#include "MyInfixCalculator.h"
+
+// using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        std::cout << "You have to provide one test";
+        std::cout << "instance file and output file name to run the test main!\n";
+        return(0);
+    }
+
+    std::ifstream inFile;
+    inFile.open(argv[1]);
+    
+    if (inFile.is_open()) {
+        std::string str;
+        std::getline(inFile, str);
+        MyInfixCalculator infix_calculator;
+        double a = infix_calculator.calculate(str);
+        std::cout.precision(3);
+        std::cout << std::fixed << a << std::endl;
+    } else {
+        std::cout << "The instance.txt file cannot be opened";
+    }
+    
+    inFile.close();
+
+    return(0);
+}
