b53f6fb168363c0bde54ebc56cd86baea055f7db
morgan bergen <morgan@ku.edu>
2023-04-10 10:24:17 -0500

    simply restructured brackets

--- src/05-hash-table/02-provided/src/MyHashTable_Template.h
+++ src/05-hash-table/02-provided/src/MyHashTable_Template.h
@@ -16,28 +16,20 @@ static const long long prime_digits = 19;     // parameter used for finding a Me
 static const long long mersenne_prime = (1 << prime_digits) - 1;  // the Mersenne prime for universal hashing
 
 // fast calculation of (n modulo mersenne_prime)
-long long fastMersenneModulo(const long long n)
-{
-    // code begins
-
-    // code ends
-}
+long long fastMersenneModulo(const long long n) { }
 
 // definition of the template hash function class
 template <typename KeyType>
-class HashFunc
-{
+class HashFunc {
   public:
     long long univHash(const KeyType key, const long long table_size) const;
 };
 
 // the hash function class that supports the hashing of the "long long" data type
 template <>
-class HashFunc<long long>
-{
+class HashFunc<long long> {
   public:
-    long long univHash(const long long key, const long long table_size) const
-    {
+    long long univHash(const long long key, const long long table_size) const {
         long long hv = fastMersenneModulo(static_cast<long long>(uh_param_a * key + uh_param_b));
         hv = hv % table_size;
         return hv;
@@ -46,16 +38,13 @@ class HashFunc<long long>
 
 // the has function class that supports the hashing of the "std::string" data type
 template <>
-class HashFunc<std::string>
-{
+class HashFunc<std::string> {
   private:
     const int param_base = 37;      // the base used for inflating each character
   public:    
-    long long univHash(const std::string& key, const long long table_size) const
-    {
+    long long univHash(const std::string& key, const long long table_size) const {
         long long hv = 0;
-        for(size_t i = 0; i < key.length(); ++ i)
-        {
+        for(size_t i = 0; i < key.length(); ++ i) {
             hv = param_base * hv + static_cast<long long>(key[i]);
         }
         hv = fastMersenneModulo(static_cast<long long>(uh_param_a * hv + uh_param_b));
@@ -66,47 +55,36 @@ class HashFunc<std::string>
 
 // definition of the template hashed object class
 template <typename KeyType, typename ValueType>
-class HashedObj
-{
+class HashedObj {
   public:
 
     KeyType key;
     ValueType value;
 
-    HashedObj()
-    {
+    HashedObj() {
         return;
     }
 
-    HashedObj(const KeyType& k, const ValueType& v) :
-        key(k),
-        value(v)
-    {
+    HashedObj(const KeyType& k, const ValueType& v) : key(k), value(v) {
         return;
     }
   
-    HashedObj(KeyType && k, ValueType && v) :
-        key(std::move(k)),
-        value(std::move(v))
-    {
+    HashedObj(KeyType && k, ValueType && v) : key(std::move(k)), value(std::move(v)) {
         return;
     }
 
-    bool operator==(const HashedObj<KeyType, ValueType>& rhs)
-    {
+    bool operator==(const HashedObj<KeyType, ValueType>& rhs) {
         return (key == rhs.key);
     }    
 
-    bool operator!=(const HashedObj<KeyType, ValueType>& rhs)
-    {
+    bool operator!=(const HashedObj<KeyType, ValueType>& rhs) {
         return !(*this == rhs);
     }   
 
 };
 
 template <typename KeyType, typename ValueType>
-class MyHashTable
-{ 
+class MyHashTable { 
   private:
     size_t theSize; // the number of data elements stored in the hash table
     MyVector<MyLinkedList<HashedObj<KeyType, ValueType> >* > hash_table;    // the hash table implementing the separate chaining approach
@@ -115,44 +93,28 @@ class MyHashTable
     // pre-calculate a set of primes using the sieve of Eratosthenes algorithm
     // will be called if table doubling requires a larger prime number for table size
     // expected to update the private member "primes"
-    void preCalPrimes(const size_t n)
-    {
-        // code begins
-
-        // code ends
+    void preCalPrimes(const size_t n) {
     }
 
     // finding the smallest prime that is larger than or equal to n
     // should perform binary search against the private member "primes"
-    size_t nextPrime(const size_t n)
-    {
-        // code begins
-
-        // code ends
+    size_t nextPrime(const size_t n) {
     }
 
     // finds the MyLinkedList itertor that corresponds to the hashed object that has the specified key
     // returns the end() iterator if not found
-    typename MyLinkedList<HashedObj<KeyType, ValueType> >::iterator find(const KeyType& key)
-    {
-        // code begins
-
+    typename MyLinkedList<HashedObj<KeyType, ValueType> >::iterator find(const KeyType& key) {
         // code ends
     }
 
     // rehashes all data elements in the hash table into a new hash table with new_size
     // note that the new_size can be either smaller or larger than the existing size
-    void rehash(const size_t new_size)
-    {
-        // code begins
-
-        // code ends
+    void rehash(const size_t new_size) {
     }
 
     // doubles the size of the table and perform rehashing
     // the new table size should be the smallest prime that is larger than the expected new table size (double of the old size)
-    void doubleTable()
-    {
+    void doubleTable() {
         size_t new_size = nextPrime(2 * hash_table.size());
         this->rehash(new_size);
         return;
@@ -160,8 +122,7 @@ class MyHashTable
 
     // halves the size of the table and perform rehahsing
     // the new table size should be the smallest prime that is larger than the expected new table size (half of the old size)
-    void halveTable()
-    {
+    void halveTable() {
         size_t new_size = nextPrime(ceil(hash_table.size() / 2));
         this->rehash(new_size);
         return;
@@ -170,83 +131,47 @@ class MyHashTable
   public:
 
     // the default constructor; allocate memory if necessary
-    explicit MyHashTable(const size_t init_size = 3)
-    {
-        // code begins
-
-        // code ends
+    explicit MyHashTable(const size_t init_size = 3) {
     }
 
     // the default destructor; collect memory if necessary
-    ~MyHashTable()
-    {
-        // code begins
-
-        // code ends
+    ~MyHashTable() {
     }
 
     // checks if the hash tabel contains the given key
-    bool contains(const KeyType& key)
-    {
-        // code begins
-
-        // code ends
+    bool contains(const KeyType& key) {
     }
 
     // retrieves the data element that has the specified key
     // returns true if the key is contained in the hash table
     // return false otherwise
-    bool retrieve(const KeyType& key, HashedObj<KeyType, ValueType>& data)
-    {
-        // code begins
-
-        // code ends
+    bool retrieve(const KeyType& key, HashedObj<KeyType, ValueType>& data) {
     }
 
     // inserts the given data element into the hash table (copy)
     // returns true if the key is not contained in the hash table
     // return false otherwise
-    bool insert(const HashedObj<KeyType, ValueType>& x)
-    {
-        // code begins
-
-        // code ends
+    bool insert(const HashedObj<KeyType, ValueType>& x) {
     }
 
     // inserts the given data element into the hash table (move)
     // returns true if the key is not contained in the hash table
     // return false otherwise
-    bool insert(HashedObj<KeyType, ValueType> && x)
-    {
-        // code begins
-
-        // code ends
+    bool insert(HashedObj<KeyType, ValueType> && x) {
     }
 
     // removes the data element that has the key from the hash table
     // returns true if the key is contained in the hash table
     // returns false otherwise
-    bool remove(const KeyType& key)
-    {
-        // code begins
-
-        // code ends
+    bool remove(const KeyType& key) {
     }
 
     // returns the number of data elements stored in the hash table
-    size_t size()
-    {
-        // code begins
-
-        // code ends
+    size_t size() {
     }
 
     // returns the capacity of the hash table
-    size_t capacity()
-    {
-        // code begins
-
-        // code ends
+    size_t capacity() {
     }
 
 };
