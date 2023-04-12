aa07731f9a5365cad2ce90ba6e7acd9264143025
morgan bergen <morgan@ku.edu>
2023-04-12 09:07:13 -0500

    dir for all chaining files

---
+++ 05-hash-table/src/chaining/hashtable.h
@@ -0,0 +1,28 @@
+#ifndef __HASHTABLE__
+#define __HASHTABLE__
+
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <functional>
+
+template <typename hashobj>
+
+class hashtable {
+    public:
+        explicit hashtable(int size = 101);
+        bool contains(const hashobj &x);
+        void makeempty();
+        bool insert(const hashobj &x);
+        bool insert(hashobj &&x);
+        bool remove(const hashobj &x);
+
+    private:
+        vector<list<hashobj>> theLists;
+        int currentSize;
+        void rehash();
+        size_t myhash(const hashobj &x);
+};
+
+#endif
---
+++ 05-hash-table/src/chaining/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include "hashtable.h"
+
+int main() {
+
+    hashtable<std::string> ht;
+
+    return(0);
+}
