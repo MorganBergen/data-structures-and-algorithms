d2fafd7bbcaf7de1ff7791a9d148ec453e803430
morgan bergen <morgan@ku.edu>
2023-05-09 21:33:00 -0500

    altered structure of braces

--- src/01-vectors/03-final/MyVector_m358b583.h
+++ src/01-vectors/03-final/MyVector_m358b583.h
@@ -5,8 +5,7 @@
 #include <vector>
 
 template <typename DataType>
-class MyVector
-{
+class MyVector {
   private:
     /* data */
     size_t theSize;                         // the number of data elements the vector is currently holding
@@ -90,8 +89,7 @@ class MyVector
     }
 
     // allocate more memory for the array
-    void reserve(size_t newCapacity)
-    {
+    void reserve(size_t newCapacity) {
         // code begins
         if (newCapacity < theSize) {
             return;
@@ -107,93 +105,72 @@ class MyVector
     }
 
     // data access operator (without bound checking)
-    DataType & operator[] (size_t index)
-    {
-        // code begins
+    DataType & operator[] (size_t index) {
         return data[index];
-        // code ends
     }
 
-    const DataType & operator[](size_t index) const
-    {
-        // code begins
+    const DataType & operator[](size_t index) const {
         return data[index];
-        // code ends
     }
 
     // check if the vector is empty; return TURE if the vector is empty
-    bool empty() const
-    {
-        // code begins
+    bool empty() const {
         if (theSize == 0) {
             return true;
         } else {
             return false;
         }
-        // code ends
     }
 
     // returns the size of the vector
-    size_t size() const 
-    {
-        // code begins
+    size_t size() const {
         return theSize;
-        // code ends
     }
 
     // returns the capacity of the vector
     size_t capacity() const {
-        // code begins
         return theCapacity;
-        // code ends
     }
 
     // insert an data element to the end of the vector
     void push_back(const DataType & x) {
-        // code begins
         if (theSize == theCapacity) {
             reserve(2 * theCapacity + 1);
         }
         data[theSize++] = x;
-        // code ends
     }
 
     void push_back(DataType && x) {
-        // code begins
         if (theSize == theCapacity) {
             reserve(2 * theCapacity + 1);
         } 
         data[theSize++] = std::move(x);
-        // code ends
     }
 
     // append a vector as indicated by the parameter to the current vector ????????????????????????
     MyVector<DataType>& append(MyVector<DataType> && rhs) {
-        // code begins
+
         if (theSize + rhs.theSize > theCapacity) {
             reserve(theSize + rhs.theSize);
         }
+
         for (int i = 0; i < rhs.theSize; ++i) {
             data[theSize + i] = std::move(rhs.data[i]);
         }
+
         theSize += rhs.theSize;
         rhs.theSize = 0;
         return (*this);
-        // code ends
     }
 
     // remove the last data element from the array
     void pop_back() {
-        // code begins
         --theSize;
-        // code ends
     }
 
     // returns the last data elemtn from the array
     const DataType& back() const {
-        // code begins
         return data[theSize - 1];
-        // code ends
     }
 
     // iterator implementation
@@ -202,30 +179,21 @@ class MyVector
     typedef const DataType* const_iterator;
 
     iterator begin() {
-        // code begins
         return &data[0];
-        // code ends
     }
 
     const_iterator begin() const {
-        // code begins
         return &data[0];
-        // code ends
     }
 
     iterator end() {
-        // code begins
         return &data[size()];
-        // code ends
     }
 
     const_iterator end() const {
-        // code begins
         return &data[size()];
-        // code ends
     }
 
 };
 
-
-#endif // __MYVECTOR_H__
+#endif // __VECTOR_H__
