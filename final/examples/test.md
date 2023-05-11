7427da772a0e6a5f361caac910f7e617b697d938
morgan bergen <morgan@ku.edu>
2023-05-11 07:12:24 -0500

    print lots assets for readme

---
+++ final/examples/test.md
@@ -0,0 +1,9 @@
+##  testing myself
+
+what are the big five defined in the context of a c++ object
+
+destructor:  :
+copy constructor
+move constructor
+copy assignment operator overloader
+move assignment operator overloader
--- other/printlots-intersect-union/printP.cpp
+++ other/printlots-intersect-union/printP.cpp
@@ -3,16 +3,13 @@
 
 using namespace std;
 
-void PrintP( MyLinkedList<int> L, MyLinkedList<int> P )
-{
+void PrintP( MyLinkedList<int> L, MyLinkedList<int> P ) {
 	auto itrL = L.begin();
 	auto itrP = P.begin();
 	int counter = 0;
 
-	while ( itrL != L.end() && itrP != P.end() )
-	{
-		if ( *itrL == counter )
-		{
+	while (itrL != L.end() && itrP != P.end()) {
+		if (*itrL == counter) {
 			cout << *itrP << endl;
 			itrL++;
 		}
@@ -21,26 +18,22 @@ void PrintP( MyLinkedList<int> L, MyLinkedList<int> P )
 	}
 }
 
-int main()
-{
+int main() {
 	MyLinkedList<int> L, P;
 
-	for ( int i = 0; i < 101; i++ )
-	{
+	for ( int i = 0; i < 101; i++ ) {
 		P.push_back( i );
 	}
 
 	int i = 0;
-	while ( i < 105 )
-	{
-		if ( i % 5 == 0 )
-		{
+	while (i < 105) {
+		if (i % 5 == 0) {
 			L.push_back( i );
 		}
 		i++;
 	}
 
-	PrintP( L, P );
+	PrintP(L, P);
 
 	return 0;
 }	
